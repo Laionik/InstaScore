0ecb08c77313dd9a9ad9b1cbc33a9ae1652f8a28
Laionik <lukasz.sosinski4@gmail.com>
2015-03-25 11:46:47 +0100

    Update AuthConfig.cs

--- InstaScore/App_Start/AuthConfig.cs
+++ InstaScore/App_Start/AuthConfig.cs
@@ -18,13 +18,13 @@ namespace InstaScore
             //    clientId: "",
             //    clientSecret: "");
 
-             OAuthWebSecurity.RegisterTwitterClient(
-                 consumerKey: "eD3sdPM3A1dOon6KuSJVFDzb3",
-                 consumerSecret: "RLo0Kqyh5yaypOVRwaFKnsftNEWxCfz7MbTa2lBRCDkknVPEdd");
+            // OAuthWebSecurity.RegisterTwitterClient(
+            //     consumerKey: "eD3sdPM3A1dOon6KuSJVFDzb3",
+            //     consumerSecret: "RLo0Kqyh5yaypOVRwaFKnsftNEWxCfz7MbTa2lBRCDkknVPEdd");
 
-            OAuthWebSecurity.RegisterFacebookClient(
-                appId: "462882370526476",
-                appSecret: "938b0d194772c891891f82384201ffd4");
+            //OAuthWebSecurity.RegisterFacebookClient(
+            //    appId: "462882370526476",
+            //    appSecret: "938b0d194772c891891f82384201ffd4");
 
             //OAuthWebSecurity.RegisterGoogleClient();
         }
