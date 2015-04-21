8284a2d9c9497c9606a343a5db0f8b9417c063be
Laionik <lukasz.sosinski4@gmail.com>
2015-04-21 10:09:57 +0200

    Porównywarka zdjęć lokalnych

--- InstaScore/Controllers/HomeController.cs
+++ InstaScore/Controllers/HomeController.cs
@@ -33,10 +33,101 @@ namespace InstaScore.Controllers
             return View();
         }
 
-        [Authorize(Roles = "admin")]
+        [Authorize(Roles = "user")]
         public ActionResult Photo()
         {
             ViewBag.PhotoMessage = "Zdjęcia dostępne w tym tygodniu";
+            return View();
+        }
+
+        [Authorize(Roles = "user")]
+        public ActionResult Compare()
+        {
+            ViewBag.ResultCMessage = "Porównaj zdjęcia";
+            var ph1x = "/Images/ex/";
+            var ph2x = "/Images/ex/";
+            Random rnd = new Random();
+            int ph1, ph2;
+            ph1 = rnd.Next(1, 8);
+            while (true)
+            {
+                ph2 = rnd.Next(1, 8);
+                if (ph2 != ph1)
+                {
+                    break;
+                }
+
+            }
+            ph1x += "ex" + ph1 + ".jpg";
+            ph2x += "ex" + ph2 + ".jpg";
+
+            List<SelectListItem> photos = new List<SelectListItem>();
+            photos.Add(new SelectListItem
+            {
+                Text = "Zdjęcie " + ph1,
+                Value = ph1.ToString()
+            });
+            photos.Add(new SelectListItem
+            {
+                Text = "Zdjęcie " + ph2,
+                Value = ph2.ToString()
+            });
+
+            ViewBag.Photos = photos;
+            ViewBag.ph1x = ph1x;
+            ViewBag.ph2x = ph2x;
+
+            /*
+             * DODAJ +1 do total dla obu zdjęć
+             */
+            return View();
+        }
+
+        [Authorize(Roles = "user")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Compare(string win)
+        {
+            win = Request.Form["Photos"];
+            ViewBag.ResultCMessage = "Wybrałeś zdjęcie o ID = " + win;
+            /*
+             * do sth at DB
+             */
+            /*
+            * DODAJ +1 do total dla obu zdjęć
+            */
+            var ph1x = "/Images/ex/";
+            var ph2x = "/Images/ex/";
+            Random rnd = new Random();
+            int ph1, ph2;
+            ph1 = int.Parse(win);
+            while (true)
+            {
+                ph2 = rnd.Next(1, 8);
+                if (ph2 != ph1)
+                {
+                    break;
+                }
+
+            }
+            ph1x += "ex" + ph1 + ".jpg";
+            ph2x += "ex" + ph2 + ".jpg";
+
+            List<SelectListItem> photos = new List<SelectListItem>();
+            photos.Add(new SelectListItem
+            {
+                Text = "Zdjęcie " + ph1,
+                Value = ph1.ToString()
+            });
+            photos.Add(new SelectListItem
+            {
+                Text = "Zdjęcie " + ph2,
+                Value = ph2.ToString()
+            });
+
+            ViewBag.Photos = photos;
+            ViewBag.ph1x = ph1x;
+            ViewBag.ph2x = ph2x;
             return View();
         }
     }
---
+++ InstaScore/Models/PhotoModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace InstaScore.Models
+{
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<PhotoModel> Photos { get; set; }
+    }
+
+    public class PhotoModel
+    {
+        public int ID { get; set; }
+        public string link { get; set; }
+        public int score { get; set; }
+        public int total { get; set; }
+    }
+}
